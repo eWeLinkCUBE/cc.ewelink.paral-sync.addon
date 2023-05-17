2f3c5fc7c52bcdcbfd9d3b22a0a2b002536f90a9
SuiKa <1061854357@qq.com>
2023-05-17 14:12:31 +0800

    补充sse部分的try catch

--- server/services/sse.ts
+++ server/services/sse.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from "express";
 import sseClass from "../ts/class/sse";
+import logger from "../log";
 
 export default async function sse(req: Request, res: Response) {
-    sseClass.buildStreamContext(req, res);
+    try {
+        sseClass.buildStreamContext(req, res);
+    } catch (err) {
+        logger.info("build sse connection error: ", err);
+    }
 }
