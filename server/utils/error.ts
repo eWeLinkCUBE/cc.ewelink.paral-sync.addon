a4eb3a752005ba08273f8577616415776a82c550
Tony Yuan <tony.yuan@coolkit.cn>
2023-05-19 15:17:19 +0800

    重构获取指定网关的设备列表接口

--- server/utils/error.ts
+++ server/utils/error.ts
@@ -25,9 +25,9 @@ export const ERR_NO_DEST_GATEWAY_MAC = 700;
 /** 无同步目标网关的信息 */
 export const ERR_NO_DEST_GATEWAY_INFO = 701; // m
 /** 同步目标网关的 IP 不可用 */
-export const ERR_DEST_GATEWAY_IP_INVALID = 702; // 1
+export const ERR_DEST_GATEWAY_IP_INVALID = 702; // 1 2
 /** 同步目标网关的凭证不可用 */
-export const ERR_DEST_GATEWAY_TOKEN_INVALID = 703;
+export const ERR_DEST_GATEWAY_TOKEN_INVALID = 703; // 2
 
 /** 无同步来源网关的信息 */
 export const ERR_NO_SRC_GATEWAY_INFO = 1500; // 1
@@ -39,9 +39,9 @@ export const ERR_SRC_GATEWAY_TOKEN_INVALID = 1502;
 export const ERR_SYNC_DEVICE_NOT_IN_SRC_GATEWAY = 1503;
 
 /** eWeLink Cube API 获取设备列表凭证无效 */
-export const ERR_CUBEAPI_GET_DEVICE_TOKEN_INVALID = 600;
+export const ERR_CUBEAPI_GET_DEVICE_TOKEN_INVALID = 600; // 2
 /** eWeLink Cube API 获取设备列表请求超时 */
-export const ERR_CUBEAPI_GET_DEVICE_TIMEOUT = 601;
+export const ERR_CUBEAPI_GET_DEVICE_TIMEOUT = 601; // 2
 /** eWeLink Cube API 添加第三方设备凭证无效 */
 export const ERR_CUBEAPI_SYNC_DEVICE_TOKEN_INVALID = 602;
 /** eWeLink Cube API 添加第三方设备请求超时 */
@@ -52,11 +52,11 @@ export const ERR_CUBEAPI_SYNC_DEVICE_PARAMS_INVALID = 604;
 export const ERR_CUBEAPI_GET_GATEWAY_TOKEN_TIMEOUT = 605; // 1
 
 /** 无相应的网关信息 */
-export const ERR_NO_SUCH_GATEWAY = 501;
+export const ERR_NO_SUCH_GATEWAY = 501; // 2
 /** 请求网关的 IP 无效 */
-export const ERR_GATEWAY_IP_INVALID = 502;
+export const ERR_GATEWAY_IP_INVALID = 502; // 2
 /** 请求网关的凭证无效 */
-export const ERR_GATEWAY_TOKEN_INVALID = 503;
+export const ERR_GATEWAY_TOKEN_INVALID = 503; // 2
 
 
 // 错误映射
